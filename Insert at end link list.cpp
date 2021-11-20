ffe1814157542be2ca561140831ad5cc320eaf46
SouvikGhosh644 <89792876+SouvikGhosh644@users.noreply.github.com>
2021-11-20 15:34:48 +0530

    Add files via upload

---
+++ Insert at end link list.cpp
@@ -0,0 +1,51 @@
+//Insert at end//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+};
+
+void insert_end(Node** head_ref,int val)
+{
+    Node* new_node=new Node();
+    new_node->data=val;
+    new_node->next=NULL;
+    if(*head_ref==NULL)
+    {
+        (*head_ref)=new_node;
+        return ;
+    }
+    
+    Node* temp=(*head_ref);
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=new_node;
+    return ;
+}
+
+void print(Node* head_ref)
+{
+    Node* temp=head_ref;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+int main()
+{
+    vector<int>arr={1,2,3,4,5};
+    Node* head=NULL;
+    for(int i=0;i<arr.size();i++)
+    {
+        insert_end(&head,arr[i]);
+    }
+    print(head);
+}
